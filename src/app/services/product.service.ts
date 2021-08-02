9ab4501cbe5a0074cfaf6c7dd24c3407973b3481
AgatheEdifixio <agathe.marcellini@edifixio.com>
2021-08-02 16:05:10 +0200

    clean code

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -19,17 +19,7 @@ export class ProductService {
   url : string = "http://localhost:3000/products";
   cartUrl : string = "http://localhost:3000/cart";
 
-  postData = {
-    test: 'my content'
-  }
-
-  productToAddToCart: Products | any
-  //retrieves all products form db
-  // récupérer un array de product => declarer dans les chevrons 
- 
-
-  //retrieves all products form db
-  // récupérer un array de product => declarer dans les chevrons 
+  
   getProducts(): Observable<Products[]>{
     return this.http.get<Products[]>("http://localhost:3000/products")
   }
@@ -38,12 +28,6 @@ export class ProductService {
     return this.http.get<Products[]>("http://localhost:3000/cart")
   }
 
-AddProductxxx(){
-this.http.post(this.cartUrl, this.postData).toPromise().then(data => {
-  console.log(data);
-
-});
-  } 
 
   //ajouter le produit : doit pouvoir en ajouter autant qu'il veut 
   AddProduct(object : Products){
